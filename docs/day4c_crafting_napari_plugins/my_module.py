c7c4f4e2f47e2a741afe2b86800dd0e9a334f371
Marcelo Zoccoler <marcelozoc@gmail.com>
2022-10-14 14:42:25 +0200

    add notebook with magicgui

--- docs/day4c_crafting_napari_plugins/my_module.py
+++ docs/day4c_crafting_napari_plugins/my_module.py
@@ -3,23 +3,11 @@ from skimage.morphology import white_tophat, disk
 from skimage.filters import gaussian, threshold_otsu
 from skimage.measure import label, regionprops_table
 from typing import List
+from pathlib import Path
 import pandas as pd
 import numpy as np
-from magicgui.widgets import Select, ListEdit
 
 from enum import Enum
-
-class RefractiveIndex(Enum):
-    Oil = 1.515
-    Water = 1.33
-    Air = 1.0
-    
-from magicgui.widgets import ComboBox, Container
-
-items = [('box1', ('a', 'b', 'c')), ('box2', ('d', 'e', 'f'))]
-widgets = [ComboBox(choices=c, label=l) for l, c in items]
-container = Container(widgets=widgets)
-    
 
 from napari.types import ImageData, LabelsData, LayerDataTuple
 
@@ -47,42 +35,18 @@ def subtract_bg_and_denoise(image: ImageData, background_subtraction_radius: int
     denoised_layer = (denoised, {"colormap": "plasma"}, "image")
     return [background_subtracted_layer, denoised_layer]
 
-def segment_image_with_features(image: ImageData, requested_measurements: RefractiveIndex) -> LayerDataTuple:
-    """Apply thresholding and connected component analysis and measure specified properties"""
-    binary = image > threshold_otsu(image)
-    labels = label(binary)
-    print(requested_measurements)
-    # regionprops = regionprops_table(image, labels, properties=requested_measurements)
-    # table = pd.DataFrame(regionprops)
-    
-    layer_properties = {"name": "segmented",
-                        # "properties": table,
-                       }
-    
-    labels_layer = (labels, layer_properties, "labels")
-    return labels_layer
-
-
-                    
-
-def extract_features(image, labels, requested_measurements):
+def extract_features(image: ImageData, labels: LabelsData, area: bool = True, mean_intensity: bool = True, output_path: str = ''):
     """Measure specified properties"""
+    requested_measurements = ['label']
+    if area:
+        requested_measurements.append('area')
+    if mean_intensity:
+        requested_measurements.append('mean_intensity')
+
     regionprops = regionprops_table(image, labels, properties=requested_measurements)
     table = pd.DataFrame(regionprops)
-    return table
+    
+    table.to_csv(Path(output_path, 'table.csv'))
+    return
 
-def analyse_average_total_intensity(filename, background_subtraction_radius = 15, particle_radius = 5):
-    """Load an image, segment objects and measure their mean total intensity."""
-    # load data
-    image = imread(filename)
-    
-    denoised = preprocess_image(image, background_subtraction_radius, particle_radius)
-    labels = segment_image(denoised)
-    
-    requested_measurements = ["area", "mean_intensity"]
-    table = extract_features(image, labels, requested_measurements)
 
-    # descriptive statistics
-    mean_total_intensity = np.mean(table["area"] * table["mean_intensity"])
-    
-    return mean_total_intensity
